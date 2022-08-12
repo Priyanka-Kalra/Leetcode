256a2a5d0f81952da1e820444f01454218c9e44c
Priyanka <100306427+Priyanka-Kalra@users.noreply.github.com>
2022-08-12 23:28:55 +0530

    Time: 80 ms (74.04%), Space: 42.4 MB (45.84%) - LeetHub

---
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    //https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/discuss/2337113/O(k)-Time-complexity-Simple-solution-with-handwritten-explanation.
+    int maxScore(vector<int>& cardPoints, int k) {
+        
+        int n=cardPoints.size();
+        int right_sum=0,left_sum=0;
+        
+        //sum of right k elements
+        for(int i=0;i<k;i++)
+            right_sum+=cardPoints[n-1-i];
+        
+        //
+        int max_score=right_sum;
+        for(int i=0;i<k;i++){
+            left_sum+=cardPoints[i];
+            right_sum-=cardPoints[n-k+i];
+            
+            max_score=max(max_score,left_sum+right_sum);
+        }
+        
+        return max_score;
+    }
+};
