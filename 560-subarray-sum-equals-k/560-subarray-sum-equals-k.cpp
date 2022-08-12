249652668b500da9b41389ce4ba64b5f39629b17
Priyanka <100306427+Priyanka-Kalra@users.noreply.github.com>
2022-08-13 00:20:53 +0530

    Time: 111 ms (79.65%), Space: 41.4 MB (78.29%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -7,24 +7,25 @@ public:
         unordered_map<int,int> mp;
         
         //prefix array
-        vector<int> PrefSum(n,0);
-        PrefSum[0]=nums[0];
-        for(int i=1;i<n;i++)
-            PrefSum[i]=PrefSum[i-1]+nums[i];
+        //vector<int> PrefSum(n,0);
+        // PrefSum[0]=nums[0];
+        // for(int i=1;i<n;i++)
+        //     PrefSum[i]=PrefSum[i-1]+nums[i];
         
-        
+        int sum=0;
         int ans=0;
         for(int i=0;i<n;i++){
             
-            int val=PrefSum[i];
-            int temp=val-k;
+            sum+=nums[i];
+            //int val=PrefSum[i];
+            int temp=sum-k;
             
-            if(val==k)ans++;
+            if(sum==k)ans++;
             
             if(mp.count(temp)>0)
                 ans+=mp[temp];
             
-            mp[val]++;
+            mp[sum]++;
         }
         return ans;
     }
