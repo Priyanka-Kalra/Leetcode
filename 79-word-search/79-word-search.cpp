98be098cd62588c989003c9b558cdda00233706c
Priyanka <100306427+Priyanka-Kalra@users.noreply.github.com>
2022-08-13 04:22:44 +0530

    Time: 866 ms (30.99%), Space: 8.1 MB (37.71%) - LeetHub

---
+++ 79-word-search/79-word-search.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    bool exist(int m,int n,int r,int c,vector<vector<char> > &board,int ptr, string word,vector<vector<int> > &visited) {
+        
+        if(word[ptr]=='\0')return true;
+        
+        if(r<0 ||r>=m ||c<0 || c>=n || word[ptr]!=board[r][c] || visited[r][c]!=-1)return false;
+        
+        visited[r][c]=0;
+        bool t= (exist(m,n,r+1,c,board,ptr+1,word,visited)|| 
+                 exist(m,n,r-1,c,board,ptr+1,word,visited)|| 
+                 exist(m,n,r,c+1,board,ptr+1,word,visited)|| 
+                 exist(m,n,r,c-1,board,ptr+1,word,visited) );
+        
+        visited[r][c]=-1;
+        return t;
+
+    }
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int m=board.size();
+        int n=board[0].size();
+        
+        vector<vector<int> > visited(m,vector<int>(n,-1));
+        
+        //find start positiom
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(exist(m,n,i,j,board,0,word,visited))
+                    return true;
+            }
+        }
+        return false;
+    }
+};
