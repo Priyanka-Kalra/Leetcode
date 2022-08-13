b99a3ac61aecf93aa6728a8c29a684202790cae4
Priyanka <100306427+Priyanka-Kalra@users.noreply.github.com>
2022-08-13 17:09:02 +0530

    Time: 26 ms (85.55%), Space: 9.1 MB (82.14%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    void combine(int si,int n, int k,vector<int> &temp,vector<vector<int>> &output){
+        
+        if(k==0){
+            output.push_back(temp);
+            return;
+        }
+        if(si>n)return;
+        
+        for(int i=si;i<=n;i++){
+            temp.push_back(i);
+            combine(i+1,n,k-1,temp,output);
+            temp.pop_back();
+        }
+        
+    }
+public:
+    vector<vector<int>> combine(int n, int k) {
+        
+        vector<int> temp;
+        vector<vector<int>> output;
+        combine(1,n,k,temp,output);
+        return output;
+    }
+};
