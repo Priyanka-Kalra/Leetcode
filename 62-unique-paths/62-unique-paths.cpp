cd02d0122287de7f86a043e2dfda41f60cb9ab2e
Priyanka <100306427+Priyanka-Kalra@users.noreply.github.com>
2022-08-21 18:40:46 +0530

    Time: 8 ms (6.65%), Space: 6.4 MB (46.14%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,3 +1,6 @@
+//1.Recursion,TC-O(2^(n*m)) ,SC-O(m+n-2)
+//2.Memoization,TC-O(m*n)  ,SC-O(m+n-2)+O(m*n)
+/*
 class Solution {
     int count(int r,int c,int m,int n,vector<vector<int> > &dp){
         
@@ -13,5 +16,27 @@ public:
         
         vector<vector<int> > dp(m,vector<int>(n,-1));
         return count(0,0,m,n,dp);
+    }
+};
+*/
+//dp
+class Solution {
+
+public:
+    int uniquePaths(int m, int n) {
+        
+        vector<vector<int> > dp(m,vector<int>(n,-1));
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0)dp[0][0]=1;
+                else{
+                    int left=(i>0)?dp[i-1][j]:0;
+                    int right=(j>0)?dp[i][j-1]:0;
+                    dp[i][j]=left+right;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
