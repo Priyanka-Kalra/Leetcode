ddbdfc46d5437493b6bebea0d11cca3c5d73a77c
Priyanka <100306427+Priyanka-Kalra@users.noreply.github.com>
2022-08-22 16:55:30 +0530

    Time: 13 ms (34.89%), Space: 10.7 MB (31.77%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.cpp
@@ -0,0 +1,42 @@
+//https://leetcode.com/problems/simplify-path/discuss/1847357/C%2B%2B-oror-Easy-oror-Stack-oror-Simple-oror-Explained-oror-Algorithm
+class Solution {
+public:
+    // "/a//b////c/d//././/.."
+    // "/a/b/c"
+    string simplifyPath(string path) {
+        
+        stack<string> order;
+        for(int i=0;path[i]!='\0';){
+            
+            if(path[i]=='/'){
+                i++;
+                continue;
+            }
+            string temp="";
+            while(path[i]!='\0' &&path[i]!='/'){
+                temp+=path[i];
+                i++;
+            }
+            
+            
+            if(temp==".."){
+                if(order.size()>0)order.pop();
+            }
+            else if(temp==".")continue;
+            else order.push(temp); 
+        }
+        
+        string ans="";
+        if(order.size()==0){
+            ans+='/';
+            return ans;
+        }
+        
+        while(!order.empty()){
+            ans='/'+order.top()+ans;
+            order.pop();
+        }
+        
+        return ans;
+    }
+};
