5512d9a5cf975bc1414be53302027d68c4261f3e
Priyanka <100306427+Priyanka-Kalra@users.noreply.github.com>
2022-08-25 22:53:01 +0530

    Time: 71 ms (36.46%), Space: 20.7 MB (56.09%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,3 +1,10 @@
+//TC-O(N*k)N is size of strs,K is avg length of each string
+//SC-O(N)
+
+/*
+1.Maintain a map with key(of map) as string type and value(of map) as vector<string>. this means "unordered_map<string,vector<string> >mp;"
+2.store each string as temp and then sort each string(s).store temp with key as s.this means "mp[s].push_back(temp)"
+*/
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
