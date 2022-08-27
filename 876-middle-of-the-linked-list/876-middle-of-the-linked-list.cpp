bae94a816c7fabeedc333384c15ca782ad1a4824
Priyanka <100306427+Priyanka-Kalra@users.noreply.github.com>
2022-08-27 22:42:13 +0530

    Time: 6 ms (19.27%), Space: 7.1 MB (22.54%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -8,6 +8,13 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//TC-O(N),SC-O(1)
+/*make two pointers slow and fast both initialised to head.now move slow by 1 node and fast by 2 nodes. slow pointer will point to middle when
+fast==NULL (IN CASE OF EVEN NUMBER OF NODES)
+fast->NEXT==NULL (IN CASE OF ODD NUMBER OF NODES)
+*/
+
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
