ab9e3bb821c9293326b4d17d831e6459198ac381
Priyanka <100306427+Priyanka-Kalra@users.noreply.github.com>
2022-08-31 19:42:29 +0530

    Time: 21 ms (90.46%), Space: 20.3 MB (27.10%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.cpp
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -9,6 +9,49 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class Solution {
+public:
+    //https://www.youtube.com/watch?v=yyZA4v0x16w
+    //similar to subarray with sum k,TC-O(N),SC-O(N)
+    int count=0;
+    void f(TreeNode* root, int targetSum,long sum,unordered_map<long,int> &mp){
+        
+        if(root==NULL)return;
+        
+        //operation on root node
+        sum+=root->val;
+        if(sum==targetSum)count++;
+        if(mp.find(sum-targetSum)!=mp.end())count+=mp[sum-targetSum];
+        mp[sum]++;
+        
+        //left and right node
+        f(root->left,targetSum,sum,mp);
+        f(root->right,targetSum,sum,mp);
+        
+        //backtrack
+        if(mp[sum]>1)mp[sum]--;
+        else mp.erase(sum);
+    }
+    /*
+    count=1
+    target=22
+    sum=20
+    mp=5,1  9,1  20,1   
+    
+    
+    */
+    int pathSum(TreeNode* root, int targetSum) {
+        
+        if(root==NULL)return 0;
+        
+        unordered_map<long,int> mp;
+        long sum=0;
+        f(root,targetSum,sum,mp);
+        return count;
+    }
+};
+/*
+
 //for each node calculate path sum,TC-O(N^2),SC-O(N)(due to recursion)
 class Solution {
 public:
@@ -33,4 +76,5 @@ public:
         return count;
         
     }
-};
+    };
+*/
