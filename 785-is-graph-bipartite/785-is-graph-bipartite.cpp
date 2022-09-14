5f842c7997157b6fa2aee57472783970b8e62a47
Priyanka <100306427+Priyanka-Kalra@users.noreply.github.com>
2022-09-15 05:08:45 +0530

    Time: 25 ms (93.20%), Space: 13.5 MB (60.99%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,30 +1,18 @@
 class Solution {
-    
-    bool bipartite_bfs(vector<vector <int> > & graph,int node,vector <int> &colour){
+    bool bipartite_dfs(vector<vector <int> > & graph,int node,int col,vector <int> &colour){
         
-        queue<pair<int,int> > order;
+        colour[node]=col;
         
-        order.push({node,0});
-        while(!order.empty() ){
+        for(int i=0;i<graph[node].size();i++){
+            int temp=graph[node][i];
             
-            pair<int,int> p=order.front();
-            order.pop();
-            
-            int val=p.first;
-            int col=p.second;
-            colour[val]=col;
-            
-            for(int i=0;i<graph[val].size();i++){
-                int temp=graph[val][i];
-                if(colour[temp]==-1)order.push({temp,1-col});
-                
-                else if(colour[temp]==col)return false;
-                
-            }
-            
+            if(colour[temp]==-1 && !bipartite_dfs(graph,temp,1-col,colour))
+                return false;
+            else if(colour[temp]==col)
+                return false;
         }
-        return true;
         
+        return true;
     }
 public:
     
@@ -39,7 +27,11 @@ public:
             if(colour[i]==-1){
                 
                 //graph,parent,colour 
-                if(!bipartite_bfs(graph,i,colour))
+                // if(!bipartite_bfs(graph,i,colour))
+                //     return false;
+                
+                //graph,parent,col,colour 
+                if(!bipartite_dfs(graph,i,0,colour))
                     return false;
             }
         }
