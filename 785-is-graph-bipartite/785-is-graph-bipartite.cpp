bf3cf86aaec54588a7998b2d2d070362cea6d3fe
Priyanka <100306427+Priyanka-Kalra@users.noreply.github.com>
2022-09-15 05:09:15 +0530

    Time: 59 ms (19.39%), Space: 13.5 MB (60.99%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,4 +1,31 @@
 class Solution {
+    //     bool bipartite_bfs(vector<vector <int> > & graph,int node,vector <int> &colour){
+        
+//         queue<pair<int,int> > order;
+        
+//         order.push({node,0});
+//         while(!order.empty() ){
+            
+//             pair<int,int> p=order.front();
+//             order.pop();
+            
+//             int val=p.first;
+//             int col=p.second;
+//             colour[val]=col;
+            
+//             for(int i=0;i<graph[val].size();i++){
+//                 int temp=graph[val][i];
+//                 if(colour[temp]==-1)order.push({temp,1-col});
+                
+//                 else if(colour[temp]==col)return false;
+                
+//             }
+            
+//         }
+//         return true;
+        
+//     }
+    
     bool bipartite_dfs(vector<vector <int> > & graph,int node,int col,vector <int> &colour){
         
         colour[node]=col;
