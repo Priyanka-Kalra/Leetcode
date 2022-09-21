5d7f347a9758a5231b422da3d0d2a618160f6aaa
Priyanka <100306427+Priyanka-Kalra@users.noreply.github.com>
2022-09-22 02:42:12 +0530

    Time: 119 ms (88.26%), Space: 46.1 MB (38.75%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -1,3 +1,8 @@
+//see leetcode solution for reference
+//TC-O(N),SC-O(1)
+//we initially maintain sum of even numbers.When changes are made to indexes having a even number then 
+//first we subtract nums[ind] from sum. then we update nums[ind],by nums[ind]+=val
+//in last we update add nums[ind](updated) to sum if updated value of nums[i] is even
 class Solution {
 public:
     vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
@@ -16,9 +21,6 @@ public:
             if(nums[ind]%2==0)sum-=nums[ind];
             nums[ind]+=val;
             if(nums[ind]%2==0)sum+=nums[ind];
-                
-    
-            
             ans.push_back(sum);  
         }
         
@@ -26,6 +28,3 @@ public:
         
     }
 };
-//[7,2]
-//[[4,0],[3,0]]
-//[2 ]
