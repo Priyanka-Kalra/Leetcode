20e7082f0250f2d25105ddc17c5eb1e5046429ae
Priyanka <100306427+Priyanka-Kalra@users.noreply.github.com>
2022-09-26 01:06:34 +0530

    Time: 8 ms (13.54%), Space: 5.9 MB (30.31%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverse(int x) {
+        
+        bool negative=x<0?1:0;
+        
+        int Y=abs(x);
+        long long invert=0;
+        while(Y>0){
+            invert=invert*10+Y%10;
+            Y/=10;
+        }
+        
+        if(negative)invert*=-1;
+        
+        if(invert>INT_MAX || invert<INT_MIN)return 0;
+        return invert;
+    }
+};
