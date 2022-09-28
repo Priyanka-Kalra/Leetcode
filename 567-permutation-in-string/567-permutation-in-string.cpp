3e8e42062acf6183dbf4a850bf6b56ca0a6683eb
Priyanka <100306427+Priyanka-Kalra@users.noreply.github.com>
2022-09-28 20:25:21 +0530

    Time: 81 ms (19.68%), Space: 29.8 MB (16.29%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -0,0 +1,37 @@
+//
+class Solution {
+    bool areEqual(vector<int> a,vector<int> b){
+        
+        for(int i=0;i<26;i++){
+            if(a[i]!=b[i])return false;
+
+        }
+        return true;
+    }
+public:
+    bool checkInclusion(string s1, string s2) {
+        
+        if(s1.size()>s2.size())return false;
+        
+        vector<int> freq1(26,0);
+        vector<int> freq2(26,0);
+        
+        for(char i:s1)freq1[i-'a']++;
+        
+        int i=0,j=0;
+        while(j<s2.size()){
+            
+            freq2[s2[j]-'a']++;
+            
+            if(j-i+1==s1.size() && areEqual(freq1,freq2))return true;
+            else if(j-i+1<s1.size())j++;
+            else{
+                freq2[s2[i]-'a']--;
+                i++;
+                j++;
+            }
+        }
+        
+        return false;
+    }
+};
