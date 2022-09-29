ddff29383907a5920711c967f1d010b67d8536ba
Priyanka <100306427+Priyanka-Kalra@users.noreply.github.com>
2022-09-29 19:30:00 +0530

    Time: 127 ms (39.78%), Space: 31.6 MB (77.75%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,3 +1,4 @@
+//TC-O(N) can be improved to O(logN+k) by using binary search
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
@@ -5,7 +6,10 @@ public:
         int n=arr.size();
         
         int ind=-1;
+        
+        //Edge cases:1.when x is lesser than first element(out of range)
         if(arr[0]>=x)ind=0;
+        //Edge cases:2.when x is greater than largest element(out of range)
         else if(arr[n-1]<x)ind=n-1;
         if(ind==-1){
             for(int i=1;i<n;i++){
@@ -14,6 +18,7 @@ public:
                     ind=i;
                     break;
                 }
+                //Edge cases:3. when x is in the range but not present in arr
                 else if(arr[i-1]<x && arr[i]>x){
                     ind=x-arr[i-1]<= arr[i]-x ?i-1:i;
                     break;
