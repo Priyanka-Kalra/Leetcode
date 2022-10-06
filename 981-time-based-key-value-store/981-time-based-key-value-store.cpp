d2e1e92d229df66899dca236049210f89d1a8eaf
Priyanka <100306427+Priyanka-Kalra@users.noreply.github.com>
2022-10-06 16:16:29 +0530

    Time: 831 ms (11.55%), Space: 128.9 MB (43.93%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -1,12 +1,16 @@
 class TimeMap {
-    
+    //key,(time,value)
     unordered_map<string, unordered_map< int,string > > mp;
 public:
+    //n is avg length of key and value
+    //t=timestamp
     
+    //O(n) for each call
     void set(string key, string value, int timestamp) {
         mp[key][timestamp]=value;
     }
     
+    //O(t.n) for each call
     string get(string key, int timestamp) {
         
         if(mp.count(key)==0)return "";
