5c863fbef2aa1228fcceebcee67d61114a8b042e
Priyanka <100306427+Priyanka-Kalra@users.noreply.github.com>
2022-10-12 09:04:17 +0530

    Time: 45 ms (84.05%), Space: 21.9 MB (22.88%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -5,13 +5,11 @@ public:
         int n=nums.size();
         sort(nums.begin(),nums.end());
         
-        int a,b,c;
+        
         for(int i=n-3;i>=0;i--){
-            a=nums[i];
-            b=nums[i+1];
-            c=nums[i+2];
+            int a=nums[i],b=nums[i+1],c=nums[i+2];
             
-            if(b+a>c)return (a+b+c);
+            if(a+b >c)return (a+b+c);
         }
         return 0;
     }
