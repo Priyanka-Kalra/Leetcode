5f94c9c5246a231b2dc5b5f22e81ddc3fb8ff40f
Priyanka <100306427+Priyanka-Kalra@users.noreply.github.com>
2022-10-25 14:28:56 +0530

    Time: 205 ms (45.86%), Space: 102.1 MB (23.64%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,29 +1,29 @@
 //using backtracking form different subsequences. for each subsequence check whether all characters unique or not(if unique update the answer)
 class Solution {
     bool isunique(string s){
-        
         vector<int> freq(26,0); 
-        for(int i=0;s[i]!='\0';i++){
-            if(freq[s[i]-'a']==1)return false;
-            else freq[s[i]-'a']++;
+        
+        for(auto i:s){
+            
+            if(freq[i-'a']==1)return false;
+            else freq[i-'a']++;
         }
         return true;
     }
+    
     void maxLength(int ind,int n,vector<string>& arr,string cur_string,int &ans){
         
         if(!isunique(cur_string))return;
         ans=max(ans, (int)cur_string.size());
         if(ind==n)return;
         
-        
         maxLength(ind+1,n,arr,cur_string+arr[ind],ans);
         maxLength(ind+1,n,arr,cur_string,ans);
-        
     }
     
 public:
+    
     int maxLength(vector<string>& arr) {
-        
         int ans=0;
         maxLength(0,arr.size(),arr,"",ans);
         return ans;
