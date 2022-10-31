d74ec79f401fadbb3bb6b2ac9c586e54bf7c216c
Priyanka <100306427+Priyanka-Kalra@users.noreply.github.com>
2022-10-31 22:20:27 +0530

    Time: 44 ms (8.41%), Space: 17.4 MB (85.40%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        
+        int m=matrix.size();
+        int n=matrix[0].size();
+        
+        //left to right first col
+        for(int i=0;i<n;i++){
+            int el=matrix[0][i];
+            int k=i+1;
+            for(int j=1;j<m&& k<n;j++){
+                if(matrix[j][k]!=el)return false;
+                k++;
+            }
+        }
+        
+        //top to down first row
+        for(int i=1;i<m;i++){
+            int el=matrix[i][0];
+            int k=i+1;
+            for(int j=1;j<n&& k<m;j++){
+                if(matrix[k][j]!=el)return false;
+                k++;
+            }
+        }
+        
+        return true;
+    }
+};
+//[36,59,71,15,26,82,87]
+//[56,36,59,71,15,26,82]
+//[15, 0,36,59,71,15,26]
+
+//  0 1 2 3
+//0
+//1
+//2
