6bc48ded54a2a364003ced87835b564c4b6443c5
Priyanka <100306427+Priyanka-Kalra@users.noreply.github.com>
2022-12-04 15:33:18 +0530

    Time: 272 ms (56.17%), Space: 90.9 MB (19.79%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -5,26 +5,26 @@ public:
         int n=nums.size();
         if(n==1)return 0;
         
-        vector<long int> pre(n);
-        vector<long int> pos(n);
+        int i,temp;
+        vector<long> pre(n);
+        vector<long> pos(n);
         
         pre[0]=nums[0];
-        for(int i=1;i<n;i++)pre[i]=nums[i]+pre[i-1];
+        for(i=1;i<n;i++)pre[i]=nums[i]+pre[i-1];
         
         pos[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--)pos[i]=nums[i]+pos[i+1];
+        for(i=n-2;i>=0;i--)pos[i]=nums[i]+pos[i+1];
         
         int minV=INT_MAX,ind=-1;
-        for(int i=0;i<n-1;i++){
-
-            int temp=abs( (pre[i]/(i+1)) -(pos[i+1]/(n-i-1)) );
+        for(i=0;i<n-1;i++){
+            temp=abs( (pre[i]/(i+1)) -(pos[i+1]/(n-i-1)) );
             if(temp<minV){
                 minV=temp;
                 ind=i;
             } 
         }
         
-        int temp=pre[n-1]/n;
+        temp=pre[n-1]/n;
         if(temp<minV)ind=n-1;
         
         return ind;
